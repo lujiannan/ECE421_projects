272a33b30329647bb4bcce8c1d390f789486a522
Brandon Hoynick <hoynick@ualberta.ca>
2024-02-16 13:51:53 -0700

    added second executable style (now it has both arg and no arg styles), updated readme, cleaned code

---
+++ README.md
@@ -0,0 +1,52 @@
+# Our Stock Querying Program
+ECE421 Group Project 1, due Feb 16, 2024  
+Program Name: Our Stock Querying Program  
+Version: 1.0.0 
+Author: Prabh Kooner, Brandon Hoynick, Jiannan Lu  
+
+## Crates Used
+- `chrono`: This crate is used for handling date and time data (like converting unix timestamps).  https://crates.io/crates/chrono
+- `clap`: This crate is used for command line argument parsing (like `--help`). https://crates.io/crates/clap
+- `plotly`: This crate is used for creating interactive charts to visualize the stock data. https://crates.io/crates/plotly
+- `tokio`: This crate is used for asynchronous programming (for async-await parsing Yahoo's stock database). https://crates.io/crates/tokio
+- `yahoo_finance_api`: This crate is used to fetch stock data from Yahoo Finance. https://crates.io/crates/yahoo_finance_api
+
+## Financial Analysis Algorithms
+As per the project requirements:
+- We used the clap library's parser to read the stock ticker from the command line and provide "--help"
+- We use a stock finder API (Yahoo) (along with tokio's async-await caller) to check for a valid given stock ticker data for last 6 months, which returns to us as a Quote variable.
+- We pull that quote variable's data into vectors to be used as points in the interactive graph; the mapping, collect, and other iterate functions allow us to keep a container variable as an array of data that can be modified (like converting unixstamp to DateTime string) all within a single line non-looped function call.
+- We use a simple for-loop that iterates over all vector data to find volatile days (using the `(highs[i] - lows[i]) / closes[i] > 0.02` calculate to signifying volatile days).
+- We calculate the highest and lowest closing prices of the last 6 months using a 'fold' min max iterator (and print those dates and prices to the console).
+- Finally, we use a simplified plotly library to populate the closing price and volatile day traces to a browser based plot.
+- Additionally, our programs prompts and calls are setup to prevent crashes (i.e. they include error checkers).
+
+## Charting Setup
+We used the `plotly` crate to create a chart of both line and candlestick traces for the stock data. The line chart shows the daily closing prices, while the candlestick represents volatile days. The x-axis represents the date and the y-axis represents the stock price (in $USD). We enabled autoscaling for the y-axis to ensure that all data points are visible in the chart. The program generates an interactive "plot.html" file. The interactivity of the chart allows the user to show/hide traces, and zoom into date ranges.
+
+## Project Setup
+To build the program:
+1. Extract any saved release (v1.0.0) zip/folder to desired location, or, 
+1. Clone the repository: `git clone https://github.com/kooner27/421_projects.git` 
+2. Navigate within the project directory to the src parent folder: `cd 421_projects/stock_monitor/`
+3. Build the project: `cargo build --release`
+4. (Optional) To directly build AND run the program, run the command: `cargo run --release -- --ticker <stock_ticker>` or `cargo run --release`
+5. (Optional) To directly build AND run the help menu: `cargo run --release -- --help`
+
+To run the compiled binary build:
+1. Navigate to the executable directory: `cd target/release/`
+2. Run the executable: `stock_monitor --ticker <stock_ticker>` or `stock_monitor`
+3. (Optional) Open the help menu: `stock_monitor --help`
+
+## Usage Instructions
+After running the program:
+1. Open the generated `plot_<stock_ticker>.html` file (in the same directory).
+2. Navigate browser chart; clicking on the top-right options (e.g. zoom) allows you to select spots on the chart to enhance (e.g. zoom: allows you to select a smaller range of dates to zoom into); Selecting the legends will show/hide the 'daily closing price' and 'volatile day' traces.
+3. The terminal will also display the dates of the highest and lowest closing prices for the last 6-months.
+
+## References
+- plotly examples - https://github.com/igiagkiozis/plotly/blob/master/examples/financial_charts/src/main.rs ; https://docs.rs/plotly/latest/plotly/plot/struct.Plot.html ; https://blog.logrocket.com/plotting-rust-projects-plotly/
+- chrono timestamp conversion example - https://www.epochconvert.com/programming/rust
+- match,OK,Err example -  https://doc.rust-lang.org/rust-by-example/error/result/result_map.html
+- unwrap, panic exmaple - https://www.programiz.com/rust/unwrap-and-expect 
+- Yahoo stock data examples - https://docs.rs/yahoo_finance_api/latest/yahoo_finance_api/#get-the-history-of-quotes-for-time-range ; https://crates.io/crates/yahoo_finance_api/0.3.1
--- stock_monitor/src/main.rs
+++ stock_monitor/src/main.rs
@@ -2,7 +2,11 @@
 ECE421 Group Project 1, due Feb 15, 2024
 
 Program Name: Our Stock Querying Program
+<<<<<<< Updated upstream
 Version: 0.1
+=======
+Version: 1.0.0
+>>>>>>> Stashed changes
 Author: Prabh Kooner, Brandon Hoynick, Jiannan Lu
 About:
     This program queries stock data from Yahoo Finance and displays it in an interactive chart.
@@ -14,8 +18,41 @@ About:
 
 */
 
+<<<<<<< Updated upstream
 use chrono::prelude::{DateTime, NaiveDateTime, Utc}; // for date and time unix conversion
 use clap::App; // for command line argument parser
+=======
+// Program Description:
+const ABOUT_TEXT: &str = r#"
+This program queries stock tickers from Yahoo Finance 
+and displays it's historical data in an interactive chart.
+
+There are two ways to use this program:
+
+1. Start the executable with a stock ticker as a --ticker argument.
+If that ticker is valid, 
+it will then query Yahoo Finance for the ticker's last 6-month's daily closing values,
+and output the values in an interactive chart (using a browser-based html file).
+The program will then exit.
+
+2. Start the executable without any arguments.
+The program will prompt you to enter a stock ticker.
+If that ticker is valid, 
+it will then query Yahoo Finance for the ticker's last 6-month's daily closing values,
+and output the values in an interactive chart (using a browser-based html file).
+The program will then ask if you would like to continue checking stocks.
+If you choose to continue, the program will prompt you to enter another stock ticker,
+otherwise you can exit the program.
+
+The chart also highlights volatile days 
+(where the difference between the high and low prices is greater than 2% of the closing price), 
+using a candlestick chart overlay option.
+"#;
+
+// Importing the necessary libraries
+use chrono::prelude::DateTime; // for date and time unix conversion
+use clap::Parser; // for command line argument parser
+>>>>>>> Stashed changes
 use plotly::common::{Marker, Mode, Title}; // for charting abilities
 use plotly::layout::{Axis, Layout};
 use plotly::{Candlestick, Plot, Scatter};
@@ -55,9 +92,43 @@ fn convert_quotes_to_candlestick_data(
     let lows: Vec<f64> = quotes.iter().map(|quote| quote.low).collect();
     let closes: Vec<f64> = quotes.iter().map(|quote| quote.close).collect();
 
-    return (dates, opens, highs, lows, closes); //return tuple of vectors ready to be used in a stock chart
+    // returns tuple of vectors ready to be used in a stock chart
+    return (dates, opens, highs, lows, closes);
 }
 
+<<<<<<< Updated upstream
+=======
+// Function to find which days of the previous vectors are volatile, and return them in a new set of vectors
+fn grab_volatile_days(
+    dates: Vec<String>,
+    opens: Vec<f64>,
+    highs: Vec<f64>,
+    lows: Vec<f64>,
+    closes: Vec<f64>,
+) -> (Vec<String>, Vec<f64>, Vec<f64>, Vec<f64>, Vec<f64>) {
+    // Let's setup some new vectors to hold the volatile days
+    let mut vdates = vec![];
+    let mut vopens = vec![];
+    let mut vhighs = vec![];
+    let mut vlows = vec![];
+    let mut vcloses = vec![];
+
+    // iterate through the dates, and if the difference between the high and low is greater than 2% of the closing price, then add it to the new vectors
+    for (i, date) in dates.iter().enumerate() {
+        if (highs[i] - lows[i]) / closes[i] > 0.02 {
+            vdates.push(date.clone());
+            vopens.push(opens[i]);
+            vhighs.push(highs[i]);
+            vlows.push(lows[i]);
+            vcloses.push(closes[i]);
+        }
+    }
+
+    // returns tuple of vectors ready to be used in a stock chart
+    return (vdates, vopens, vhighs, vlows, vcloses);
+}
+
+>>>>>>> Stashed changes
 // Function to setup the plotly chart
 fn setup_plotly_chart(quotes: Vec<Quote>, stock_ticker: &String) {
     // pull out the dates, opens, highs, lows, and closes from the quotes and put them into separate vectors
@@ -84,6 +155,7 @@ fn setup_plotly_chart(quotes: Vec<Quote>, stock_ticker: &String) {
         .marker(Marker::new().size(4)) // Set the marker size
         .name("Daily Closing Prices"); // Set the trace name
 
+<<<<<<< Updated upstream
     // Of the given days, let's find the volatile dates //* should put this in a separate function
     let mut vdates = vec![];
     let mut vopens = vec![];
@@ -100,6 +172,11 @@ fn setup_plotly_chart(quotes: Vec<Quote>, stock_ticker: &String) {
             vcloses.push(closes[i]);
         }
     }
+=======
+    // Of the given days, let's find the volatile dates
+    let (vdates, vopens, vhighs, vlows, vcloses) =
+        grab_volatile_days(dates, opens, highs, lows, closes);
+>>>>>>> Stashed changes
 
     // Create a second trace (in Candlestick form) to represent volatile dates
     let trace_2_error_volatile_days =
@@ -121,6 +198,7 @@ fn setup_plotly_chart(quotes: Vec<Quote>, stock_ticker: &String) {
         .height(900); // Set the height to 800 pixels (default is 450 pixels, which is too short)
     plot.set_layout(layout);
 
+<<<<<<< Updated upstream
     plot.show(); // open the plot in a browser window
 }
 
@@ -199,5 +277,103 @@ async fn main() {
                 println!("Invalid input, please try again.");
             }
         }
+=======
+    // saves the plot to an html file
+    plot.write_html(&format!("plot_{}.html", stock_ticker));
+}
+
+#[tokio::main] // allows async await function (when using Yahoo to check for stock data)
+async fn stock_data_checker(provider: &yahoo::YahooConnector, stock_ticker_arg: &String) {
+    // Function to check for stock data using Yahoo
+    match provider // look for quote, using 'match,OK,Err' style
+        .get_quote_range(&stock_ticker_arg, "1d", "6mo")
+        .await
+    {
+        // check Yahoo for this ticker, for daily values, for last 6-months
+        Ok(response) => {
+            // if we get a good ticker...
+            match response.quotes() {
+                // ...then get the quotes...
+                Ok(quotes) => {
+                    // println!("We have received a valid stock ticker response from: {}", quotes.longname); //* print the longname of the stock (cant seem find it in the quotes struct)
+                    setup_plotly_chart(quotes, &stock_ticker_arg); // ...and use the quotes in a candlestick chart. Ref:
+                }
+                Err(e) => println!(
+                    "Error: {}, Failed to get quotes for ticker: {}",
+                    e, stock_ticker_arg
+                ),
+            }
+        }
+        Err(e) => println!("Error: {}, Invalid ticker: {}", e, stock_ticker_arg),
+    }
+}
+
+// this is needed to use clap, cmd line parsing, about, and help
+#[derive(Parser, Debug)]
+#[command(version = "1.0.0", about = ABOUT_TEXT , long_about = None)]
+struct Args {
+    /// Stock ticker
+    #[arg(short, long)]
+    ticker: Option<String>,
+}
+
+// main function to run the program
+fn main() {
+    let arg_present; // used to check if a ticker was passed as an argument
+
+    // parse args (if any)
+    let args = Args::parse();
+    let stock_ticker_arg = match args.ticker {
+        Some(ticker) => {
+            // if a ticker is present, use it
+            let ticker = ticker.trim().to_uppercase();
+            println!("Your stock ticker: {}", ticker);
+            arg_present = true;
+            ticker
+        }
+        None => {
+            // if no ticker is present, keep the program running to ask the user for a ticker
+            arg_present = false;
+            String::new() // blank unused ticker, but satisfies the compiler
+        }
+    };
+
+    let provider = yahoo::YahooConnector::new(); // setup stock data provider (Yahoo)
+
+    // check if a ticker was passed as an argument
+    if arg_present == true {
+        stock_data_checker(&provider, &stock_ticker_arg); // check for the stock ticker data using the provider
+    } else {
+        // if no ticker was passed as an argument, keep the program running to ask the user for a ticker
+        println!("Welcome to our stock querying program!");
+        let mut while_holder = true; // keep the program running until the user decides to exit
+        while while_holder {
+            println!("Enter a valid stock ticker to get the last 6-month's daily values: ");
+            let mut stock_ticker = String::new();
+            let _ = std::io::stdin().read_line(&mut stock_ticker); // get keyboard input
+            stock_ticker = stock_ticker.trim().to_uppercase(); // make it trimmed and uppercase
+            println!("Your stock ticker: {}", stock_ticker);
+
+            stock_data_checker(&provider, &stock_ticker); // check for the stock ticker data using the provider
+
+            let mut while_holder_2 = true; // keep the program running until the user decides to exit
+            while while_holder_2 {
+                println!("Would you like to continue checking stocks? (y or n): ");
+                let mut ask = String::new();
+                let _ = std::io::stdin().read_line(&mut ask); // get keyboard input
+                ask = ask.trim().to_uppercase(); // make it trimmed and uppercase
+                if ask == "Y" || ask == "YES" {
+                    while_holder = true;
+                    while_holder_2 = false;
+                } else if ask == "N" || ask == "NO" {
+                    while_holder = false;
+                    while_holder_2 = false;
+                    println!("Thank you for using our stock querying program, goodbye!");
+                } else {
+                    println!("Invalid input, please try again.");
+                }
+            }
+        }
+>>>>>>> Stashed changes
     }
 }
