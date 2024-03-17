5518fe7a0eef201dfa6437d543d2a80d0c879802
kooner27 <prabhnoo@ualberta.ca>
2024-03-17 02:19:52 -0600

    asdf

--- trees/src/main.rs
+++ trees/src/main.rs
@@ -7,38 +7,17 @@ fn main() {
     let root: std::rc::Rc<std::cell::RefCell<TreeNode<u32>>> = TreeNode::new(0);
     let pointer = TreeNode::regular_insert(&root, 50, NodeColor::Black);
     TreeNode::regular_insert(&root, 25, NodeColor::Red);
-    TreeNode::regular_insert(&root, 100, NodeColor::Black);
-    TreeNode::regular_insert(&root, 5, NodeColor::Black);
+    TreeNode::regular_insert(&root, 100, NodeColor::Black);;
     TreeNode::regular_insert(&root, 40, NodeColor::Red);
-
-    TreeNode::regular_insert(&root, 39, NodeColor::Black);
-    TreeNode::regular_insert(&root, 41, NodeColor::Black);
-
-    
-    
-    
-
     let lr = TreeNode::lr_rotate(&pointer.unwrap());
     // lr.unwrap().borrow().print_tree();
     root.borrow().print_tree();
-    
-
-
-
-    
-    // root.borrow().print_tree();
-    
-    
-    // println!("{:#?}", result);
-
-    // result.unwrap().borrow().print_tree();
-
-    // let step1 = TreeNode::rr_rotate(&pointer.unwrap());
-    // let p = step1.unwrap().clone();
-    // let result = TreeNode::ll_rotate(&root);
-    // result.unwrap().borrow().print_tree();
-    
 }
+
+
+
+
+
 
 fn main2() {
     // Create the root of the tree with a specific key
