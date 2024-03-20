636df4b94096208fdb31fc9784e87858efba15d8
Brandon Hoynick <hoynick@ualberta.ca>
2024-03-19 19:49:20 -0600

    add function in main to test program from CLI (not as cargo run input, but as exe then get input)

--- trees/src/main.rs
+++ trees/src/main.rs
@@ -1,4 +1,6 @@
-extern crate trees; //
+extern crate trees;
+
+//
 use trees::rbtree::*;
 
 // Assuming RBTree is structured properly and has an `insert` method.
@@ -132,23 +134,23 @@ fn main1002() {
     myrbtree.insert(30);
     myrbtree.insert(40);
     myrbtree.print_tree();
-        // should be:
+    // should be:
     /*
-        ┌── 40 (Red)30
-    ┌── 30 (Black)20
-┌── 20 (Black)
-│   └── 10 (Black)20
-        */
+            ┌── 40 (Red)30
+        ┌── 30 (Black)20
+    ┌── 20 (Black)
+    │   └── 10 (Black)20
+            */
     myrbtree.insert(50);
     myrbtree.print_tree();
-        // should be:
+    // should be:
     /*
-        ┌── 50 (Red)40
-    ┌── 40 (Black)20
-    │   └── 30 (Red)40
-┌── 20 (Black)
-│   └── 10 (Black)20
-        */
+            ┌── 50 (Red)40
+        ┌── 40 (Black)20
+        │   └── 30 (Red)40
+    ┌── 20 (Black)
+    │   └── 10 (Black)20
+            */
 }
 
 fn main1003() {
@@ -163,21 +165,21 @@ fn main1003() {
     myrbtree.print_tree();
     // should be:
     /*
-    ┌── 30 (Black)20
-┌── 20 (Black)
-│   └── 10 (Black)20
-│       └── 5 (Red)10
-        */
+        ┌── 30 (Black)20
+    ┌── 20 (Black)
+    │   └── 10 (Black)20
+    │       └── 5 (Red)10
+            */
     myrbtree.insert(6);
     myrbtree.print_tree();
     // should be:
     /*
-    ┌── 30 (Black)20
-┌── 20 (Black)
-│   │   ┌── 10 (Red)6
-│   └── 6 (Black)20
-│       └── 5 (Red)6
-        */
+        ┌── 30 (Black)20
+    ┌── 20 (Black)
+    │   │   ┌── 10 (Red)6
+    │   └── 6 (Black)20
+    │       └── 5 (Red)6
+            */
 }
 
 fn main1004() {
@@ -192,21 +194,21 @@ fn main1004() {
     myrbtree.print_tree();
     // should be:
     /*
-        ┌── 40 (Red)30
-    ┌── 30 (Black)20
-┌── 20 (Black)
-│   └── 10 (Black)20
-        */
+            ┌── 40 (Red)30
+        ┌── 30 (Black)20
+    ┌── 20 (Black)
+    │   └── 10 (Black)20
+            */
     myrbtree.insert(35);
     myrbtree.print_tree();
     // should be:
     /*
-        ┌── 40 (Red)35
-    ┌── 35 (Black)20
-    │   └── 30 (Red)35
-┌── 20 (Black)
-│   └── 10 (Black)20
-        */
+            ┌── 40 (Red)35
+        ┌── 35 (Black)20
+        │   └── 30 (Red)35
+    ┌── 20 (Black)
+    │   └── 10 (Black)20
+            */
 }
 
 fn main1005() {
@@ -241,4 +243,182 @@ fn main1005() {
     ┌── 20 (Black)
     │   └── 10 (Black)20
         */
+}
+
+fn main1006() {
+    // tester for CLI program with inputs
+    println!("Welcome to our AVL or red-black tree tester program!");
+    let mut program_on = true;
+    while program_on {
+        let mut while_stage_holder_1 = true;
+        let mut tree_type_holder = String::new();
+        while while_stage_holder_1 {
+            println!("What tree type would you like to test, or, exit program (avl, rb, exit): ");
+            let mut tree_type = String::new();
+            let _ = std::io::stdin().read_line(&mut tree_type); // get keyboard input
+            tree_type = tree_type.trim().to_lowercase(); // make it trimmed and lowercase
+            println!("Your tree type: {}", tree_type);
+            match tree_type.as_str() {
+                "avl" => {
+                    println!("You have chosen AVL tree.");
+                    while_stage_holder_1 = false;
+                    tree_type_holder = "avl".to_string();
+                }
+                "rb" => {
+                    println!("You have chosen red-black tree.");
+                    while_stage_holder_1 = false;
+                    tree_type_holder = "rb".to_string();
+                }
+                "exit" => {
+                    println!("Exiting program. Goodbye!");
+                    return;
+                }
+                _ => {
+                    println!("Invalid input, please try again.");
+                }
+            }
+        }
+        let mut while_stage_holder_2 = true;
+        if tree_type_holder == "avl" {
+            let mut myavltree = RBTree::new();
+            while while_stage_holder_2 {
+                println!("What would you like to do with this AVL tree: ");
+                println!(
+                    "Commands are: insert <value>, delete <value>, leaves (i.e. count leaves),"
+                );
+                println!("height (i.e. get tree height), inorder (i.e. prints in order traversal), preorder (i.e. prints pre order traversal),");
+                println!("ifempty (i.e. check if tree is empty), print (i.e. print tree in structured format), exit");
+                let mut input = String::new();
+                let _ = std::io::stdin().read_line(&mut input); // get keyboard input
+                input = input.trim().to_lowercase(); // make it trimmed and lowercase
+                let parts: Vec<&str> = input.split_whitespace().collect();
+                match parts[0] {
+                    "insert" => {
+                        if parts.len() < 2 {
+                            println!("Expected a number after 'insert'");
+                            continue;
+                        }
+                        let num = match parts[1].parse::<u32>() {
+                            Ok(num) => num,
+                            Err(_) => {
+                                println!("Invalid input, please enter a number.");
+                                continue;
+                            }
+                        };
+                        myavltree.insert(num);
+                        println!("Inserted {}", num);
+                    }
+                    "delete" => {
+                        if parts.len() < 2 {
+                            println!("Expected a number after 'delete'");
+                            continue;
+                        }
+                        let num = match parts[1].parse::<u32>() {
+                            Ok(num) => num,
+                            Err(_) => {
+                                println!("Invalid input, please enter a number.");
+                                continue;
+                            }
+                        };
+                        myavltree.delete(num);
+                        println!("Deleted {}", num);
+                    }
+                    "leaves" => {
+                        myavltree.count_number_of_leaves();
+                    }
+                    "height" => {
+                        myavltree.get_height_of_tree();
+                    }
+                    "inorder" => {
+                        myavltree.print_in_order_traversal();
+                    }
+                    "preorder" => {
+                        myavltree.print_pre_order_traversal();
+                    }
+                    "ifempty" => {
+                        myavltree.is_tree_empty();
+                    }
+                    "print" => {
+                        myavltree.print_tree();
+                    }
+                    "exit" => {
+                        println!("Exiting program. Goodbye!");
+                        program_on = false;
+                        break;
+                    }
+                    _ => println!("Unknown command, please try again."),
+                }
+            }
+        } else if tree_type_holder == "rb" {
+            let mut myrbtree = RBTree::new();
+            while while_stage_holder_2 {
+                println!("What would you like to do with this Red-Black tree: ");
+                println!(
+                    "Commands are: insert <value>, delete <value>, leaves (i.e. count leaves),"
+                );
+                println!("height (i.e. get tree height), inorder (i.e. prints in order traversal), preorder (i.e. prints pre order traversal),");
+                println!("ifempty (i.e. check if tree is empty), print (i.e. print tree in structured format), exit");
+                let mut input = String::new();
+                let _ = std::io::stdin().read_line(&mut input); // get keyboard input
+                input = input.trim().to_lowercase(); // make it trimmed and lowercase
+                let parts: Vec<&str> = input.split_whitespace().collect();
+                match parts[0] {
+                    "insert" => {
+                        if parts.len() < 2 {
+                            println!("Expected a number after 'insert'");
+                            continue;
+                        }
+                        let num = match parts[1].parse::<u32>() {
+                            Ok(num) => num,
+                            Err(_) => {
+                                println!("Invalid input, please enter a number.");
+                                continue;
+                            }
+                        };
+                        myrbtree.insert(num);
+                        println!("Inserted {}", num);
+                    }
+                    "delete" => {
+                        if parts.len() < 2 {
+                            println!("Expected a number after 'delete'");
+                            continue;
+                        }
+                        let num = match parts[1].parse::<u32>() {
+                            Ok(num) => num,
+                            Err(_) => {
+                                println!("Invalid input, please enter a number.");
+                                continue;
+                            }
+                        };
+                        myrbtree.delete(num);
+                        println!("Deleted {}", num);
+                    }
+                    "leaves" => {
+                        myrbtree.count_number_of_leaves();
+                    }
+                    "height" => {
+                        myrbtree.get_height_of_tree();
+                    }
+                    "inorder" => {
+                        myrbtree.print_in_order_traversal();
+                    }
+                    "preorder" => {
+                        myrbtree.print_pre_order_traversal();
+                    }
+                    "ifempty" => {
+                        myrbtree.is_tree_empty();
+                    }
+                    "print" => {
+                        myrbtree.print_tree();
+                    }
+                    "exit" => {
+                        println!("Exiting program. Goodbye!");
+                        program_on = false;
+                        break;
+                    }
+                    _ => println!("Unknown command, please try again."),
+                }
+            }
+        }
+    }
 }
