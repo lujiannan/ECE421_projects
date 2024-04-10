9bd75ebd3dda6bd65c39127ec1b9bcb0c1b3149e
kooner27 <prabhnoo@ualberta.ca>
2024-04-10 17:25:38 -0600

    yew setup and rocket on port 3000

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,19 +1,32 @@
-// At the top of your main.rs
-mod connect4;
-use connect4::{Board, Player, State};
+// // At the top of your main.rs
+// mod connect4;
+// use connect4::{Board, Player, State};
 
-fn main() {
-    // Create a new game board
-    let mut board = Board::new(6, 7); // Assuming a standard Connect Four board size
-
-    
-    board.insert_disc(3);
-    board.display();
-
-    /*
-    make a command line. loop to handle game logic to test the module
-    
-     */
+// fn main() {
+//     // Create a new game board
+//     let mut board = Board::new(6, 7); // Assuming a standard Connect Four board size
 
     
+//     board.insert_disc(3);
+//     board.display();
+
+//     /*
+//     make a command line. loop to handle game logic to test the module
+    
+//      */
+
+    
+// }
+
+#[macro_use] extern crate rocket;
+
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, world from Rocket!"
 }
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![index])
+}
+
