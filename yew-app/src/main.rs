069ea8d0aa8f5c40c3f5c88d52fd3884d45faebd
Jonas Lu <nannan001204@gmail.com>
2024-04-12 18:32:38 -0600

    fix bug of move prediction

--- yew-app/src/main.rs
+++ yew-app/src/main.rs
@@ -558,7 +558,7 @@ fn connect_four_game() -> Html {
                                                     connect4::Cell::Empty => {
                                                         // UI prediction of the future position of next move
                                                         if let Some(pos) = *predicted_pos {
-                                                            if row == pos.0 && col == pos.1 { html! { <img style="opacity:0.6" src={current_player_icon} width="80" height="80"/> } }
+                                                            if row == pos.0 && col == pos.1 && is_enabled { html! { <img style="opacity:0.6" src={current_player_icon} width="80" height="80"/> } }
                                                             else {html! {}}
                                                         } else {
                                                             html! {}
